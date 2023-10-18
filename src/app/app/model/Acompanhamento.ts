7b623ca758ce8252d07bda0d2bd634a2485825df
Pedro Henrique <pedrohenri1606@gmail.com>
2023-10-18 20:51:50 -0300

    Configurado a service de Acompanhamento

--- src/app/app/model/Acompanhamento.ts
+++ src/app/app/model/Acompanhamento.ts
@@ -1,4 +1,4 @@
-export class Acompnhamento {
+export class Acompanhamento {
 
     id!: number;
     cadastro!: Date;
--- src/app/app/service/acompanhamento/acompanhamento.service.ts
+++ src/app/app/service/acompanhamento/acompanhamento.service.ts
@@ -1,9 +1,67 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Acompanhamento } from '../../model/Acompanhamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AcompanhamentoService {
 
+  API: String = "http://localhost:8080/acompanhamento"
+  
+  http = inject(HttpClient);
+
   constructor() { }
+
+  buscarPorId(id: number){
+    return this.http.get<Acompanhamento>(this.API + `/buscar?id=${id}`)
+  }
+
+  buscarOndeDescricao(conteudo: string): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/buscar/descricao?conteudo=${conteudo}`);
+  }
+
+  buscarOndeValor(valor: number): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/buscar/valor?conteudo=${valor}`);
+  }
+
+  buscarAcompanhamentoComecando(conteudo: string): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/buscar/comecando?conteudo=${conteudo}`);
+  }
+
+  buscarAcompanhamentoTerminando(conteudo: string): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/buscar/terminando?conteudo=${conteudo}`);
+  }
+
+  buscarAcompanhamentoContendo(conteudo: string): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/buscar/contendo?conteudo=${conteudo}`);
+  }
+
+  listar(): Observable<Acompanhamento[]>{
+    return this.http.get<Acompanhamento[]>(this.API + `/listar`);
+  }
+
+  cadastrar(acompanhamento: Partial<Acompanhamento>){
+    return this.http.post<Acompanhamento>(this.API + `` , acompanhamento);
+  }
+
+  editar(acompanhamentoNovo: Partial<Acompanhamento>){
+    return this.http.put<Acompanhamento>(this.API + `/editar?id=${acompanhamentoNovo.id}`, acompanhamentoNovo);
+  }
+
+  deletar(id: number){
+    return this.http.delete<Acompanhamento>(this.API + `/deletar?id=${id}`);
+  }
+
+
+  salvar(acompanhamento: Partial<Acompanhamento>){
+    if(acompanhamento.id){
+      return this.editar(acompanhamento);
+
+    } else {
+      return this.cadastrar(acompanhamento);
+    }
+  }
+  
 }
