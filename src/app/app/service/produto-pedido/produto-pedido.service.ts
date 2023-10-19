8a8a94d8149f13e283dcd661927e92d0e778df34
Pedro Henrique <pedrohenri1606@gmail.com>
2023-10-18 22:32:57 -0300

    Configurado a service de ProdutoPedido

--- src/app/app/service/produto-pedido/produto-pedido.service.ts
+++ src/app/app/service/produto-pedido/produto-pedido.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ProdutoPedido } from '../../model/ProdutoPedido';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoPedidoService {
 
+  API: string = "http://localhost:8080/produto-pedido"
+
+  http = inject(HttpClient);
+
   constructor() { }
+
+  cadastrar(produtoPedido: Partial<ProdutoPedido>){
+    return this.http.post<ProdutoPedido>(this.API + ``, produtoPedido);
+  }
+
+  buscarPorId(id: number){
+    return this.http.get<ProdutoPedido>(this.API + `/buscar?id=${id}`);
+  }
+
+  listar(): Observable<ProdutoPedido[]>{
+    return this.http.get<ProdutoPedido[]>(this.API + `/listar`);
+  }
+
+  editar(produtoPedidoNovo: Partial<ProdutoPedido>){
+    return this.http.put<ProdutoPedido>(this.API + `/editar?id=${produtoPedidoNovo.id}`, produtoPedidoNovo);
+  }
+
+  deletar(id: number){
+    return this.http.delete<ProdutoPedido>(this.API + `/deletar?id=${id}`);
+  }
+
+  salvar(produtoPedido: Partial<ProdutoPedido>){
+    if(produtoPedido.id){
+      return this.editar(produtoPedido);
+
+    } else {
+      return this.cadastrar(produtoPedido);
+    }
+  }
 }
