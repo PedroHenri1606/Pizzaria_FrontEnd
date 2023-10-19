0a8135bd905d5b225cf3da0f7826756539b796a0
Pedro Henrique <pedrohenri1606@gmail.com>
2023-10-18 21:29:52 -0300

    Configurado a service de Endereço

--- src/app/app/service/endereco/endereco.service.ts
+++ src/app/app/service/endereco/endereco.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Endereco } from '../../model/Endereco';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnderecoService {
 
+  API: string = "http://localhost:8080/endereco"
+
+  http = inject(HttpClient);
+
   constructor() { }
+
+  cadastrar(endereco: Partial<Endereco>){
+    return this.http.post<Endereco>(this.API + ``, endereco);
+  }
+
+  buscarPorId(id: number){
+    return this.http.get<Endereco>(this.API + `/buscar/id=${id}`);
+  }
+
+  listar(): Observable<Endereco[]>{
+    return this.http.get<Endereco[]>(this.API + `/listar`);
+  }
+
+  editar(enderecoNovo: Partial<Endereco>){
+    return this.http.put<Endereco>(this.API + `/editar?id=${enderecoNovo.id}`, enderecoNovo);
+  }
+
+  deletar(id: number){
+    return this.http.delete<Endereco>(this.API + `/deletar?id=${id}`);
+  }
+
+  salvar(endereco: Partial<Endereco>){
+    if(endereco.id){
+      return this.editar(endereco);
+
+    } else {
+      return this.cadastrar(endereco);
+    }
+  }
 }
