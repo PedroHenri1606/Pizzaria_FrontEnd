ee39f101544975fae4fc8234ef939f143f714fe7
Pedro Henrique <pedrohenri1606@gmail.com>
2023-10-21 18:49:52 -0300

    Iniciado implementação SaborList

--- src/app/app/components/sabor/sabordetails/sabordetails.component.ts
+++ src/app/app/components/sabor/sabordetails/sabordetails.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Sabor } from 'src/app/app/model/Sabor';
+import { SaborService } from 'src/app/app/service/sabor/sabor.service';
 
 @Component({
   selector: 'app-sabordetails',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class SabordetailsComponent {
 
+  @Input() sabor: Sabor = new Sabor();
+  @Output() retorno = new EventEmitter<Sabor>();
+
+  service = inject(SaborService);
+
+  constructor(){}
+
+  cadastrar(){
+    this.service.salvar(this.sabor).subscribe({
+      next: sabor => {
+        this.retorno.emit(sabor);
+      },
+      error: erro => {
+        console.log(erro);
+        alert("Erro no details!");
+      }
+    })
+  }
 }
--- src/app/app/components/sabor/saborlist/saborlist.component.ts
+++ src/app/app/components/sabor/saborlist/saborlist.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Sabor } from 'src/app/app/model/Sabor';
+import { SaborService } from 'src/app/app/service/sabor/sabor.service';
 
 @Component({
   selector: 'app-saborlist',
@@ -7,4 +10,62 @@ import { Component } from '@angular/core';
 })
 export class SaborlistComponent {
 
+  
+  modalService = inject(NgbModal);
+  service = inject(SaborService);
+
+  lista: Sabor[] = [];
+
+  indiceSelecionado!: number;
+  saborSelecionado!: Sabor;
+
+  constructor(){
+    this.listarTodos();
+  }
+
+  listarTodos(){
+    this.service.listar().subscribe({
+      next: lista => {
+        this.lista = lista;
+      },
+      error: erro => {
+        alert("Erro no ts sabor list!");
+      }
+    });
+  }
+
+  adicionar(modal: any){
+    this.saborSelecionado = new Sabor();
+    this.modalService.open(modal, {size: 'lg'});
+  }
+
+  editar(modal: any, id: number, produto: Sabor){
+    this.indiceSelecionado = id;
+    this.saborSelecionado = produto;
+    this.modalService.open(modal, {size: 'lg'});
+  }
+
+  buscarPorId(modal: any, id: number){
+    this.indiceSelecionado = id;
+    this.saborSelecionado = this.lista[id -1];
+    this.modalService.open(modal, {size: 'lg'});
+  }
+
+  deletar(produto: Sabor){
+    this.service.deletar(produto.id).subscribe(() => {
+      this.listarTodos();
+    });
+  }
+
+  salvar(produto: Sabor){
+    if(produto.id > 0){
+      this.lista[this.indiceSelecionado] = produto;
+    
+    } else {
+      this.lista.push(produto);
+    }
+
+    this.modalService.dismissAll();
+    this.listarTodos();
+  }
 }
